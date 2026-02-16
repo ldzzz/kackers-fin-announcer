9e1fc21a05b366041df49384907ee87202b6a8ae
Thijs van Berkel <m.p.j.v.berkel@student.tue.nl>
2026-02-16 18:47:55 +0100

    removed faster discord command loading

--- kackers_fin_announcer/bot.py
+++ kackers_fin_announcer/bot.py
@@ -32,7 +32,7 @@ class KackersFinAnnouncer(commands.Bot):
 
     async def on_ready(self):
         if not self.synced:
-            sync = await self.tree.sync(guild=self.server)
+            sync = await self.tree.sync()
             logger.info(sync)
             self.synced = True
 
