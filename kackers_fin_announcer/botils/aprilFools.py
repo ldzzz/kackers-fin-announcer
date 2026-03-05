3b638ee547ec59bf8eb19be7af80be6983694143
Thijs van Berkel <m.p.j.v.berkel@student.tue.nl>
2026-03-05 10:45:50 +0100

    added more messages

--- kackers_fin_announcer/botils/aprilFools.py
+++ kackers_fin_announcer/botils/aprilFools.py
@@ -1,6 +1,7 @@
 import random
 import botils.shelfer as std
 from botils.player_stats import get_kr_missing
+from botils.utils import parse_ts
 
 async def sendMessage(channel):
     print("April fools LO")
@@ -14,10 +15,17 @@ async def sendMessage(channel):
 
     print(f"Fooling {player}")
 
+    index = 0
     while(len(missing) == 0):
         player = random.choice(list(data.keys()))
         print(f"Fooling {player}")
         missing = get_kr_missing(data[player])
+        index += 1
+
+        if (index > 100):
+            return
+        
+    data[player]['kr_finishes'].sort(key = lambda x: x["createdAt"])
 
     difficultiesFile = open("C:/Users/20223626/OneDrive - TU Eindhoven/Documents/Programming(SELF)/kackers-fin-announcer/data/difficultyList.txt")
     difficulties = difficultiesFile.readlines()
@@ -28,5 +36,12 @@ async def sendMessage(channel):
             easiestMissing = difficulties[i]
             break
 
-    await channel.send(f"{player} is still missing {easiestMissing} LOL")
+    responsesList = [
+        f"Did u know, {player} is still missing {easiestMissing}",
+        f"KEKL {player} is still missing {easiestMissing}",
+        f"{player} is only on {len(data[player]['kr_finishes'])} finishes, laugh at them",
+        f"{player} has not finished anything since {parse_ts(data[player]['kr_finishes'][len(data[player]['kr_finishes']) - 1]['createdAt'])}"
+    ]
+
+    await channel.send(random.choice(responsesList))
 
